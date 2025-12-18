456d60e9cdcfe9cd6e2a743fd24f0e88b24ef4cf
daxina1978 <925484644@qq.com>
2025-12-18 21:01:22 +0800

    fix(bff): AppVersionInfo json fixed

--- internal/bff-service/model/response/app_version.go
+++ internal/bff-service/model/response/app_version.go
@@ -3,6 +3,6 @@ package response
 type AppVersionInfo struct {
 	Version     string `json:"version"`
 	Desc        string `json:"desc"`
-	CreatedAt   string `json:"created_at"`
-	PublishType string `json:"publish_type"`
+	CreatedAt   string `json:"createdAt"`
+	PublishType string `json:"publishType"`
 }
